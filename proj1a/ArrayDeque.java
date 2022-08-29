9bddd54c37b7e921798c12faaa734eac8e4f3907
维尼娟 <qiyue20h@qq.com>
2022-08-29 14:04:20 +0800

    6 version

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,30 +1,28 @@
-public class ArrayDeque<T>
-{
+public class ArrayDeque<T> {
     private T[] ar;
     private int size;
-    private int  head;
+    private int head;
     private int end;
-    /** head is index of first item */
-    public ArrayDeque()
-    {
-        ar =(T[]) new Object[8];
+
+    /**
+     * head is index of first item
+     */
+    public ArrayDeque() {
+        ar = (T[]) new Object[8];
         head = 0;
         end = 0;
         size = 0;
     }
-    public ArrayDeque(ArrayDeque other)
-    {
-        if (other != null)
-        {
+
+    public ArrayDeque(ArrayDeque other) {
+        if (other != null) {
             this.ar = (T[]) new Object[other.ar.length];
-            System.arraycopy(other.ar,0,this.ar,0,ar.length);
+            System.arraycopy(other.ar, 0, this.ar, 0, ar.length);
             this.head = other.head;
             this.end = other.end;
             this.size = other.size;
-        }
-        else
-        {
-            ar =(T[]) new Object[8];
+        } else {
+            ar = (T[]) new Object[8];
             head = 0;
             end = 0;
             size = 0;
@@ -35,71 +33,54 @@ public class ArrayDeque<T>
      * compute the index too;
      * */
 
-    /** in resize function head can't equal to end
-     * */
-    private void resize(double factor)
-    {
-        int newLength = (int)(ar.length*factor);
+    /**
+     * in resize function head can't equal to end
+     */
+    private void resize(double factor) {
+        int newLength = (int) (ar.length * factor);
         T[] temp = (T[]) new Object[newLength];
-        if (head < end)
-        {
-            System.arraycopy(ar,head,temp,0,size);
-        }
-        else
-        {
-            System.arraycopy(ar,head,temp,0,size - end - 1);
-            System.arraycopy(ar,0,temp,size - end - 1,end + 1);
+        if (head < end) {
+            System.arraycopy(ar, head, temp, 0, size);
+        } else {
+            System.arraycopy(ar, head, temp, 0, size - end - 1);
+            System.arraycopy(ar, 0, temp, size - end - 1, end + 1);
         }
 //        System.arraycopy(ar,head,temp,0,ar.length-head);
 //        System.arraycopy(ar,0,temp,ar.length-head,head);
         ar = temp;
     }
-    public void addFirst(T item)
-    {
-        if (size == 0)
-        {
+
+    public void addFirst(T item) {
+        if (size == 0) {
             ar[0] = item;
             size++;
-        }
-        else
-        {
-            if (size == ar.length)
-            {
+        } else {
+            if (size == ar.length) {
                 resize(2);
             }
             size++;
-            if (head == 0)
-            {
+            if (head == 0) {
                 head = ar.length - 1;
-            }
-            else
-            {
+            } else {
                 head--;
             }
             ar[head] = item;
         }
 
     }
-    public void addLast(T item)
-    {
-        if (size == 0)
-        {
+
+    public void addLast(T item) {
+        if (size == 0) {
             ar[0] = item;
             size++;
-        }
-        else
-        {
-            if (size == ar.length)
-            {
+        } else {
+            if (size == ar.length) {
                 resize(2);
             }
             size++;
-            if (end == ar.length - 1)
-            {
+            if (end == ar.length - 1) {
                 end = 0;
-            }
-            else
-            {
+            } else {
                 end++;
             }
             ar[end] = item;
@@ -114,112 +95,76 @@ public class ArrayDeque<T>
 //            ar[head+size-1-ar.length] = item;
 //        }
     }
-    public boolean isEmpty()
-    {
+
+    public boolean isEmpty() {
         return size == 0;
     }
-    public T removeFirst()
-    {
-        if (size == 0)
-        {
+
+    public T removeFirst() {
+        if (size == 0) {
             return null;
         }
-        if ((double)size/ ar.length < 0.25 && ar.length >= 16)
-        {
+        if ((double) size / ar.length < 0.25 && ar.length >= 16) {
             resize(0.5);
         }
         size--;
         T temp = ar[head];
         ar[head] = null;
-        if (head == ar.length - 1)
-        {
+        if (head == ar.length - 1) {
             head = 0;
-        }
-        else
-        {
+        } else {
             head++;
         }
         return temp;
     }
-    public T removeLast()
-    {
-        if (size == 0)
-        {
+
+    public T removeLast() {
+        if (size == 0) {
             return null;
         }
-        if ((double)size/ ar.length < 0.25 && ar.length >= 16)
-        {
+        if ((double) size / ar.length < 0.25 && ar.length >= 16) {
             resize(0.5);
         }
         size--;
         T temp = ar[end];
         ar[end] = null;
-        if (end == 0)
-        {
-            end = ar.length -1;
-        }
-        else
-        {
+        if (end == 0) {
+            end = ar.length - 1;
+        } else {
             end--;
         }
         return temp;
     }
-    public T get(int index)
-    {
-        if (index < 0 || index >= size)
-        {
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
             return null;
-        }
-        else if (head + index > ar.length - 1)
-        {
+        } else if (head + index > ar.length - 1) {
             return ar[head + index - ar.length];
-        }
-        else
-        {
+        } else {
             return ar[head + index];
         }
     }
-    public int size()
-    {
+
+    public int size() {
         return size;
     }
-    public void printDeque()
-    {
-        if (end > head)
-        {
-            for (int i = head; i <= end; i++)
-            {
-                System.out.print(ar[i]+" ");
-            }
-            System.out.println("");
-        }
-        else if (end < head)
-        {
-            for (int i = head; i < ar.length; i++)
-            {
-                System.out.println(ar[i]+" ");
-            }
-            for (int i = 0; i <= end; i++)
-            {
-                System.out.println(ar[i]+" ");
-            }
-            System.out.println("");
-        }
-    }
-    public static void main(String[] args)
-    {
-        ArrayDeque<String> ar = new ArrayDeque<>();
-        ar.removeFirst();
-        ar.addLast("haha");
-        ar.addLast("li");
-        ar.addFirst("wangwu");
-        ar.printDeque();
-        System.out.println(ar.size());
-        ar.removeFirst();
-        ar.addLast("zansan");
-        ar.removeLast();
-        System.out.println(ar.get(2));
 
+    public void printDeque() {
+        if (end > head) {
+            for (int i = head; i <= end; i++) {
+                System.out.print(ar[i] + " ");
+            }
+            System.out.println("");
+        } else if (end < head) {
+            for (int i = head; i < ar.length; i++) {
+                System.out.println(ar[i] + " ");
+            }
+            for (int i = 0; i <= end; i++) {
+                System.out.println(ar[i] + " ");
+            }
+            System.out.println("");
+        }
     }
 }
 /**
@@ -231,4 +176,4 @@ public class ArrayDeque<T>
  * public T removeFirst(): Removes and returns the item at the front of the deque. If no such item exists, returns null.
  * public T removeLast(): Removes and returns the item at the back of the deque. If no such item exists, returns null.
  * public T get(int index): Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth. If no such item exists, returns null. Must not alter the deque!
- * */
+ */
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,80 +1,79 @@
-public class LinkedListDeque<T>
-{
-    public static class Node<T>
-    {
+public class LinkedListDeque<T> {
+    public static class Node<T> {
         public T data;
         public Node<T> next, prev;
-        public Node(T d,Node<T> p,Node<T> n)
-        {
+
+        private Node(T d, Node<T> p, Node<T> n) {
             data = d;
             prev = p;
             next = n;
         }
     }
+
     private Node<T> head;
     private Node<T> end;
     private int size;
 
-    /** sentry node is to prevent sentry.next become null.next ,so we should new node to sentry
+    /**
+     * sentry node is to prevent sentry.next become null.next ,so we should new node to sentry
      * and don't set sentry.data and sentry.next = null because sentry is null if sentry don't new;
-     * */
-    public LinkedListDeque()
-    {
-        head = new Node<T>(null,null,null);
-        end = new Node<T>(null,null,null);
+     */
+    public LinkedListDeque() {
+        head = new Node<T>(null, null, null);
+        end = new Node<T>(null, null, null);
         head.next = end;
         end.prev = head;
         size = 0;
     }
-    public LinkedListDeque(LinkedListDeque l)
-    {
-//        head = new Node<T>(null,null,null);
-//        end = new Node<T>(null,null,null);
-//        head.next = end.prev;
-//        size = 0;
-        this();
-        if (l != null)
-        {
-            Node<T> temp = l.head.next;
-            for (int i = 0; i < l.size; i++)
-            {
-                addLast(temp.data);
-                temp = temp.next;
-            }
-        }
-    }
-    public void addFirst(T item)
-    {
+
+    //    public LinkedListDeque(LinkedListDeque other)
+//    {
+////        head = new Node<T>(null,null,null);
+////        end = new Node<T>(null,null,null);
+////        head.next = end.prev;
+////        size = 0;
+//        this();
+//        if (other != null)
+//        {
+//            Node<T> temp = other.head.next;
+//            for (int i = 0; i < other.size; i++)
+//            {
+//                addLast(temp.data);
+//                temp = temp.next;
+//            }
+//        }
+//    }
+    public void addFirst(T item) {
         size++;
-        head.next = new Node<>(item,head,head.next);
+        head.next = new Node<>(item, head, head.next);
         head.next.next.prev = head.next;
     }
-    public void addLast(T item)
-    {
+
+    public void addLast(T item) {
         size++;
-        end.prev = new Node<>(item,end.prev,end);
+        end.prev = new Node<>(item, end.prev, end);
         end.prev.prev.next = end.prev;
     }
-    public boolean isEmpty()
-    {
+
+    public boolean isEmpty() {
         return size == 0;
     }
-    public int size()
-    {
+
+    public int size() {
         return size;
     }
-    public void printDeque()
-    {
+
+    public void printDeque() {
         Node<T> temp = head.next;
         while (temp.next != null)  // also can use size
         {
-            System.out.print(temp.data+" ");
+            System.out.print(temp.data + " ");
             temp = temp.next;
         }
         System.out.println("");
     }
-    public T removeFirst()
-    {
+
+    public T removeFirst() {
         size--;
         T temp = head.next.data;
         head.next.data = null; // actually this is no useful because garbage collect will eat this node;
@@ -82,44 +81,39 @@ public class LinkedListDeque<T>
         head.next.prev = head;
         return temp;
     }
-    public T removeLast()
-    {
+
+    public T removeLast() {
         size--;
         T temp = end.prev.data;
         end.prev = end.prev.prev;
         end.prev.next = end;
         return temp;
     }
-    public T get(int index)
-    {
-        if (index < 0 || index >= size)
-        {
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
             return null;
         }
         Node<T> temp = head.next;
-        for (int i = 0; i < index; i++)
-        {
+        for (int i = 0; i < index; i++) {
             temp = temp.next;
         }
         return temp.data;
     }
-    public T getRecursive(int index)
-    {
-        if (index < 0 || index >= size)
-        {
+
+    public T getRecursive(int index) {
+        if (index < 0 || index >= size) {
             return null;
         }
-        return getRecursive(index,head.next);
-    }
-    private T getRecursive(int index,Node<T> node)
-    {
-        if (index == 0)
-        {
-            return node.data;
-        }
-        return getRecursive(index - 1,node.next);
+        return getRecursive(index, head.next);
     }
 
+    private T getRecursive(int index, Node<T> node) {
+        if (index == 0) {
+            return node.data;
+        }
+        return getRecursive(index - 1, node.next);
+    }
 
 
 }
@@ -132,7 +126,11 @@ public class LinkedListDeque<T>
  * public T removeFirst(): Removes and returns the item at the front of the deque. If no such item exists, returns null.
  * public T removeLast(): Removes and returns the item at the back of the deque. If no such item exists, returns null.
  * public T get(int index): Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth. If no such item exists, returns null. Must not alter the deque!
-* */
+ * public LinkedListDeque(LinkedListDeque other): Creates a deep copy of other.
+ * Creating a deep copy means that you create an entirely new LinkedListDeque, with the exact same items as other. However, they should be different objects, i.e. if you change other, the new LinkedListDeque you created should not change as well. (Edit 2/6/2018: A walkthrough that provides a solution for this copy constructor is available at https://www.youtube.com/watch?v=JNroRiEG7U4)
+ * public T getRecursive(int index): Same as get, but uses recursion.
+ * You may add any private helper classes or methods in LinkedListDeque.java if you deem it necessary.
+ */
 /**public LinkedListDeque(LinkedListDeque other): Creates a deep copy of other.
  Creating a deep copy means that you create an entirely new LinkedListDeque, with the exact same items as other. However, they should be different objects, i.e. if you change other, the new LinkedListDeque you created should not change as well. (Edit 2/6/2018: A walkthrough that provides a solution for this copy constructor is available at https://www.youtube.com/watch?v=JNroRiEG7U4)
  public T getRecursive(int index): Same as get, but uses recursion.
